d12bedadb9f76da36effeda0c941f248df11dad1
Parv Gupta <Parvg555@gmail.com>
2023-03-10 23:35:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
+//First create a list of all bold items 
+var bold_Items;
+window.onload = getBold_items();
+ 
+// Collect all <strong> tags
 function getBold_items() {
-    //Write your code here
-    
-    
+    bold_Items = document.getElementsByTagName('strong'); 
 }
 
-
+// iterate all bold tags and change color  
 function highlight() {
-    //Write your code here
-
-
+    for (var i=0; i<bold_Items.length; i++)                                                   
+        bold_Items[i].style.color = "green";
 }
 
-
+// On mouse out highlighted words become black
 function return_normal() {
-    //Write your code here
-
-    
+    for (var i=0; i<bold_Items.length; i++) 
+        bold_Items[i].style.color = "black";
 }
